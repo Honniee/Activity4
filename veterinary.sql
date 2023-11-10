fe56bbf1a8c122b8828efd876462c74b3b68323a
DiannahMae <diannahmae11@gmail.com>
2023-11-10 13:39:09 +0800

    feat/list-total-sales

--- veterinary.sql
+++ veterinary.sql
@@ -166,3 +166,8 @@ WHERE dfirstname = 'Dr. Sofia';
 -- feat/list-species-catered
 SELECT DISTINCT species
 FROM animals;
+
+-- feat/list-total-sales
+--QUERY
+SELECT SUM(totalamount) AS total_sales
+FROM invoices;
